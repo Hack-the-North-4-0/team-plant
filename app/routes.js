998dafa7aa836242c0711c0873084593952e3629
James Linton <jameslinton@Jamess-MacBook-Pro-3.local>
2020-02-13 17:23:09 +0000

    Added basic route structure.

--- app/routes.js
+++ app/routes.js
@@ -1,6 +1,31 @@
-const express = require('express')
-const router = express.Router()
+'use strict';
+
+const express = require('express');
+const router = express.Router();
 
 // Add your routes here - above the module.exports line
+router.get('/photo-id', function(req, res) {
+    let question = {
+        text : 'Do you have a photo id?',
+        name : 'photo-id',
+        next : 'bank-account'
+    };
+    res.render('question-page', question);
+});
 
-module.exports = router
+router.post('/photo-id', function(req, res) {
+    res.redirect('/bank-account');
+});
+
+router.get('/bank-account', function(req, res) {
+    let question = {
+        text : 'Do you have a bank account?',
+        name : 'bank-account',
+        next : 'nino'
+    };
+    res.render('question-page', question);
+});
+
+
+
+module.exports = router;
